ffe58bce62173260c38e3c5016fc3da8e3c5f07a
Quangbuu Le <quangbuule@gmail.com>
2015-11-09 19:46:01 +0700

    Initial Commit

---
+++ rotten-tomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rotten-tomatoes
+//
+//  Created by Lê Quang Bửu on 11/9/15.
+//  Copyright © 2015 Lê Quang Bửu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
